e4e71f6fad52919dbc23d8603fbeb9ca1d3cbe7a
braunalex74 <braunalex74@googlemail.com>
2023-04-29 20:36:50 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { PageTitle } from './PageTitle/PageTitle';
+import { EventBoard } from './EventBoard/EventBoard';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +10,11 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <PageTitle tetx="24th Core Worlds Coalition Conference" />
+      <EventBoard />
     </div>
   );
 };
