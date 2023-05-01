409db7569dd45fb1e3889f368764e54ed0d34a4e
braunalex74 <braunalex74@googlemail.com>
2023-05-01 19:39:40 +0300

    test

--- src/components/PageTitle/PageTitle.jsx
+++ src/components/PageTitle/PageTitle.jsx
@@ -1,8 +1,9 @@
 import { PropTypes } from 'prop-types';
 import css from './PageTitle.module.css';
+import { Title } from './PageTitle.styled';
 
 export const PageTitle = ({ text }) => {
-  return <h1 className={css.title}>{text}</h1>;
+  return <Title className={css.title}>{text}</Title>;
 };
 
 PageTitle.prototypes = {
